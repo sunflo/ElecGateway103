e6659a119da38091561e4f25e1a78d0bb4bbeaaa
huangxz <huangxiaozheng@kedacom.com>
2019-06-13 19:18:40 +0800

    修复警告信息

--- src/main/java/com/heshun/dsm/GateWayServerMain.java
+++ src/main/java/com/heshun/dsm/GateWayServerMain.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.swing.JList;
-import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 import com.alibaba.fastjson.JSONException;
@@ -24,105 +23,95 @@ import com.heshun.dsm.util.ELog;
 
 public class GateWayServerMain {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ControlPanel panel = new ControlPanel(new OnClickListener() {
+        ControlPanel panel = new ControlPanel(new OnClickListener() {
 
-			@Override
-			public void onInit(TextArea contentText, final Label countLabel, final TextArea tvDatas,
-					final TextField tfPort, final JList<String> mJList) {
-				ELog.getInstance().setOutputSource(contentText);
-				final ListPanel listPanel = new ListPanel(mJList);
+            @Override
+            public void onInit(TextArea contentText, final Label countLabel, final TextArea tvDatas,
+                               final TextField tfPort, final JList<String> mJList) {
+                ELog.getInstance().setOutputSource(contentText);
+                final ListPanel listPanel = new ListPanel(mJList);
+                try {
+                    SystemHelper.loadSystemConfig();
+                    SystemHelper.initMessageListener(new OnStatusChangeListener() {
+                        @Override
+                        public void onConnectChange() {
+                            countLabel.setText(String.format("online:%s", SystemHelper.minaAcceptor
+                                    .getManagedSessions().size()));
+                            listPanel.update();
+                        }
 
-				// listPanel.initJListPanel();
-				/*
-				 * 初始化监听程序
-				 */
-				try {
-					/*
-					 * 初始化服务地址
-					 */
-					SystemHelper.loadSystemConfig();
-					SystemHelper.initMessageListener(new OnStatusChangeListener() {
+                        @Override
+                        public void onDataChanged() {
+                            SwingUtilities.invokeLater(new Runnable() {
 
-						@Override
-						public void onConnectChange() {
-							countLabel.setText(String.format("online:%s", SystemHelper.minaAcceptor
-									.getManagedSessions().size()));
-							listPanel.upData();
-						}
+                                @Override
+                                public void run() {
+                                    tvDatas.setText("");
+                                    StringBuilder sb = new StringBuilder("缓冲区数据:");
 
-						@Override
-						public void onDataChanged() {
-							SwingUtilities.invokeLater(new Runnable() {
+                                    Map<Integer, Map<Integer, AbsJsonConvert<?>>> mBuffer = DataBuffer.getInstance()
+                                            .getBuffer();
+                                    sb.append(mBuffer.size()).append("\r\n");
+                                    for (Entry<Integer, Map<Integer, AbsJsonConvert<?>>> entry : mBuffer.entrySet()) {
+                                        sb.append(entry.getKey()).append(":").append(entry.getValue().size())
+                                                .append("\r\n");
+                                    }
+                                    tvDatas.setText(sb.toString());
 
-								@Override
-								public void run() {
-									tvDatas.setText("");
-									StringBuffer sb = new StringBuffer("缓冲区数据:");
+                                }
+                            });
+                        }
 
-									Map<Integer, Map<Integer, AbsJsonConvert<?>>> mBuffer = DataBuffer.getInstance()
-											.getBuffer();
-									sb.append(mBuffer.size()).append("\r\n");
-									for (Entry<Integer, Map<Integer, AbsJsonConvert<?>>> entry : mBuffer.entrySet()) {
-										sb.append(entry.getKey()).append(":").append(entry.getValue().size())
-												.append("\r\n");
-									}
-									tvDatas.setText(sb.toString());
+                    });
+                } catch (IOException e) {
+                    String errorMessage = String.format("开启Tcp监听失败，请检查%s端口是否被占用", Constants.TCP_ACCEPTOR_PORT);
+                    ELog.getInstance().err(errorMessage);
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    String errorMessage = "json解析错误，请检查配置文件格式是否正确";
+                    ELog.getInstance().err(errorMessage);
+                    e.printStackTrace();
+                }
+            }
 
-								}
-							});
-						}
+            /*
+             * 启动按钮
+             */
+            @Override
+            public void onStart() {
 
-					});
-				} catch (IOException e) {
-					String errorMessage = String.format("开启Tcp监听失败，请检查%s端口是否被占用", Constants.TCP_ACCEPTOR_PORT);
-					ELog.getInstance().err(errorMessage);
-					e.printStackTrace();
-					return;
-				} catch (JSONException e) {
-					String errorMessage = String.format("json解析错误，请检查配置文件格式是否正确", Constants.TCP_ACCEPTOR_PORT);
-					ELog.getInstance().err(errorMessage);
-					e.printStackTrace();
-				}
-			}
+                /*
+                 * 开启udp广播自身ip,等待从机的tcp连接
+                 */
+                SystemHelper.start();
+            }
 
-			/*
-			 * 启动按钮
-			 */
-			@Override
-			public void onStart() {
+            /*
+             * 停止按钮
+             */
+            @Override
+            public void onStop() {
+                System.exit(0);
+            }
 
-				/*
-				 * 开启udp广播自身ip,等待从机的tcp连接
-				 */
-				SystemHelper.start();
-			}
+            /*
+             * 清空缓存按钮
+             */
+            @Override
+            public void onFlush() {
+                SystemHelper.mQueryTask.flush();
+            }
 
-			/*
-			 * 停止按钮
-			 */
-			@Override
-			public void onStop() {
-				System.exit(0);
-			}
+            @Override
+            public void onReset() {
+            }
 
-			/*
-			 * 清空缓存按钮
-			 */
-			@Override
-			public void onFlush() {
-				SystemHelper.mQueryTask.flush();
-			}
+        });
 
-			@Override
-			public void onReset() {
-			}
+        panel.open();
 
-		});
-
-		panel.open();
-
-	}
+    }
 
 }
