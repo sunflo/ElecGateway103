e9454375aad3bb0cdd32f3b874f94640ef9a3a31
huangxz <huangxiaozheng@kedacom.com>
2019-08-29 17:25:48 +0800

    新增进度条实现

--- src/main/java/com/heshun/dsm/handler/IoMessageHandler.java
+++ src/main/java/com/heshun/dsm/handler/IoMessageHandler.java
@@ -2,6 +2,7 @@ package com.heshun.dsm.handler;
 
 import java.util.HashMap;
 
+import com.heshun.dsm.service.TotalQueryEmitter;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
@@ -20,113 +21,112 @@ import com.heshun.dsm.util.SessionUtils;
 
 /**
  * 事件处理handler
- * 
+ *
  * @author huangxz
- * 
  */
 public class IoMessageHandler extends IoHandlerAdapter {
 
-	private boolean isFirstConnect = true;
+    private boolean isFirstConnect = true;
 
-	private OnStatusChangeListener mListener;
+    private OnStatusChangeListener mListener;
 
-	private ItfTransferHandler mTransHandler;
+    private ItfTransferHandler mTransHandler;
 
-	public IoMessageHandler(OnStatusChangeListener listener) {
-		this.mListener = listener;
-		mTransHandler = new BatchTransferHandlerRefactor(mListener);
-		// mTransHandler = new BatchTransferHandler(mListener);
+    public IoMessageHandler(OnStatusChangeListener listener) {
+        this.mListener = listener;
+        mTransHandler = new BatchTransferHandlerRefactor(mListener);
+        // mTransHandler = new BatchTransferHandler(mListener);
 
-	}
+    }
 
-	@Override
-	public synchronized void messageReceived(IoSession session, Object message) throws Exception {
-		if (!(message instanceof AbsJsonConvert))
-			return;
-		if (message instanceof LogoTypeConvert) {
-			LogoTypeConvert pack = (LogoTypeConvert) message;
-			// 绑定logotype
-			SessionUtils.setLogoType(pack.logotype, session);
-			if (SessionUtils.getDevices(session) == null) {
+    @Override
+    public synchronized void messageReceived(IoSession session, Object message) throws Exception {
+        if (!(message instanceof AbsJsonConvert))
+            return;
+        if (message instanceof LogoTypeConvert) {
+            LogoTypeConvert pack = (LogoTypeConvert) message;
+            // 绑定logotype
+            SessionUtils.setLogoType(pack.logotype, session);
+            if (SessionUtils.getDevices(session) == null) {
 
-				HashMap<Integer, Device> deviceMap = SystemHelper.loadDevicesByLogoType(pack.logotype);
-				if (null == deviceMap) {
-					ELog.getInstance().err(
-							String.format("未找到配置文件，强制关闭:(<%s>%s-->%s)", session.getId(),
-									SessionUtils.getIpFromSession(session), SessionUtils.getPortFromSession(session)));
-					session.closeNow();
-				} else
-					session.setAttribute("devices", deviceMap);
-				ELog.getInstance().log(String.format("sessionId:%s==logotype:%s", session.getId(), pack.logotype));
-			}
+                HashMap<Integer, Device> deviceMap = SystemHelper.loadDevicesByLogoType(pack.logotype);
+                if (null == deviceMap) {
+                    ELog.getInstance().err(
+                            String.format("未找到配置文件，强制关闭:([%s]  %s : %s)", pack.logotype,
+                                    SessionUtils.getIpFromSession(session), SessionUtils.getPortFromSession(session)));
+                    session.closeNow();
+                } else
+                    session.setAttribute("devices", deviceMap);
+                ELog.getInstance().log(String.format("sessionId:%s && logotype:%s", session.getId(), pack.logotype));
+            }
 
-			if (isFirstConnect) {
-				if (mTransHandler instanceof BatchTransferHandlerRefactor) {
-					SystemHelper.initTotalQuery();
+            if (isFirstConnect) {
+                if (mTransHandler instanceof BatchTransferHandlerRefactor) {
+                    SystemHelper.initTotalQuery(mListener);
 
-				}
-				isFirstConnect = false;
-			}
-		} else if (message instanceof DefaultConvert) {
-			// do nothing
-		} else
-			mTransHandler.transfer(session, (AbsJsonConvert<?>) message);
+                }
+                isFirstConnect = false;
+            }
+        } else if (message instanceof DefaultConvert) {
+            // do nothing
+        } else
+            mTransHandler.transfer(session, (AbsJsonConvert<?>) message);
 
-	}
+    }
 
-	@Override
-	public void messageSent(IoSession session, Object message) throws Exception {
-		super.messageSent(session, message);
-	}
+    @Override
+    public void messageSent(IoSession session, Object message) throws Exception {
+        super.messageSent(session, message);
+    }
 
-	@Override
-	public void sessionClosed(IoSession session) throws Exception {
-		if (mListener != null) {
-			mListener.onConnectChange();
-		}
-		super.sessionClosed(session);
-	}
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+        if (mListener != null) {
+            mListener.onConnectChange();
+        }
+        super.sessionClosed(session);
+    }
 
-	@Override
-	public void sessionCreated(IoSession session) throws Exception {
-		// ELog.getInstance().log("sessionCreated", session);
-		super.sessionCreated(session);
-	}
+    @Override
+    public void sessionCreated(IoSession session) throws Exception {
+        // ELog.getInstance().log("sessionCreated", session);
+        super.sessionCreated(session);
+    }
 
-	@Override
-	public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
-		session.write(Command.getLogoTypeCommand());
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        session.write(Command.getLogoTypeCommand());
 
-		if (mListener != null) {
-			mListener.onConnectChange();
-		}
-		super.sessionIdle(session, status);
-	}
+        if (mListener != null) {
+            mListener.onConnectChange();
+        }
+        super.sessionIdle(session, status);
+    }
 
-	@Override
-	public void sessionOpened(IoSession session) throws Exception {
-		// ELog.getInstance().log(
-		// "sessionOpened:(" + session.getId() + ")" +
-		// Utils.getIpFromSession(session) + "--->"
-		// + Utils.getPortFromSession(session));
+    @Override
+    public void sessionOpened(IoSession session) throws Exception {
+        // ELog.getInstance().log(
+        // "sessionOpened:(" + session.getId() + ")" +
+        // Utils.getIpFromSession(session) + "--->"
+        // + Utils.getPortFromSession(session));
 
-		session.write(Command.getLogoTypeCommand());
+        session.write(Command.getLogoTypeCommand());
 
-		if (mListener != null) {
-			mListener.onConnectChange();
-		}
+        if (mListener != null) {
+            mListener.onConnectChange();
+        }
 
-		super.sessionOpened(session);
-	}
+        super.sessionOpened(session);
+    }
 
-	@Override
-	public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
-		ELog.getInstance().log("exception:".concat(cause.toString()), session);
-		super.exceptionCaught(session, cause);
-	}
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        ELog.getInstance().log("exception:".concat(cause.toString()), session);
+        super.exceptionCaught(session, cause);
+    }
 
-	@Override
-	public void inputClosed(IoSession session) throws Exception {
-		super.inputClosed(session);
-	}
+    @Override
+    public void inputClosed(IoSession session) throws Exception {
+        super.inputClosed(session);
+    }
 }
--- src/main/java/com/heshun/dsm/ui/ControlPanel.java
+++ src/main/java/com/heshun/dsm/ui/ControlPanel.java
@@ -9,11 +9,7 @@ import java.awt.event.WindowEvent;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 import com.heshun.dsm.common.Constants;
 import com.heshun.dsm.util.ELog;
@@ -21,184 +17,193 @@ import com.heshun.dsm.util.ELog;
 import static java.awt.TextArea.SCROLLBARS_VERTICAL_ONLY;
 
 public class ControlPanel {
-	// static ControlPanel window = null;
-	private JFrame mFrame;
-	private JPanel mPanel = new JPanel();
-	// java.awt.Label contentLable = new java.awt.Label();
-	private Button mBtnStart = new Button();
-	private Button mBtnStop = new Button();
-	private Button mBtnClear = new Button();
-	private Button mBtnLock = new Button();
-	private Button mBtnFlush = new Button();
-	private TextArea mTvLogs = new TextArea("",0,0,SCROLLBARS_VERTICAL_ONLY);
-	// private TextArea mTvDevices = new TextArea();
-	private TextArea mTvDatas = new TextArea();
-	//
-	private JList<String> mJList = new JList<>();
-	private JScrollPane mScrollPane = new JScrollPane(mJList);
-	private TextField mTfPort = new TextField();
+    // static ControlPanel window = null;
+    private JFrame mFrame;
+    private JPanel mPanel = new JPanel();
+    // java.awt.Label contentLable = new java.awt.Label();
+    private Button mBtnStart = new Button();
+    private Button mBtnStop = new Button();
+    private Button mBtnClear = new Button();
+    private Button mBtnLock = new Button();
+    private Button mBtnFlush = new Button();
+    private TextArea mTvLogs = new TextArea("", 0, 0, SCROLLBARS_VERTICAL_ONLY);
+    // private TextArea mTvDevices = new TextArea();
+    private TextArea mTvDatas = new TextArea();
+    //
+    private JList<String> mJList = new JList<>();
+    private JScrollPane mScrollPane = new JScrollPane(mJList);
+    private TextField mTfPort = new TextField();
 
-	private Label mLbCount = new Label();
-	private OnClickListener clickListener;
+    private JProgressBar mProgressBar = new JProgressBar();
+    private Label mProgressCount = new Label();
 
-	// private boolean isStart = false;
+    private Label mLbCount = new Label();
+    private OnClickListener clickListener;
 
-	private boolean isLock = false;
+    // private boolean isStart = false;
 
-	public void open() {
-		try {
-			mFrame.setTitle(String.format("DSM数据采集网关  %s ====>%s", Constants.GATEWAY_VERSION, new SimpleDateFormat(
-					"yyyy-MM-dd HH:mm").format(new Date())));
-			mFrame.setResizable(false);
-			mFrame.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private boolean isLock = false;
 
-	/**
-	 * Create the application
-	 */
-	public ControlPanel(OnClickListener clickListener) {
-		this.clickListener = clickListener;
-		initialize();
-		clickListener.onInit(mTvLogs, mLbCount, mTvDatas, mTfPort, mJList);
-	}
+    public void open() {
+        try {
+            mFrame.setTitle(String.format("DSM数据采集网关  %s ====>%s", Constants.GATEWAY_VERSION, new SimpleDateFormat(
+                    "yyyy-MM-dd HH:mm").format(new Date())));
+            mFrame.setResizable(false);
+            mFrame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Initialize the contents of the frame
-	 */
-	private void initialize() {
+    /**
+     * Create the application
+     */
+    public ControlPanel(OnClickListener clickListener) {
+        this.clickListener = clickListener;
+        initialize();
+        clickListener.onInit(mTvLogs, mLbCount, mTvDatas, mTfPort, mJList, mProgressBar, mProgressCount);
+    }
 
-		mFrame = new JFrame() {
-			private static final long serialVersionUID = 1L;
+    /**
+     * Initialize the contents of the frame
+     */
+    private void initialize() {
 
-			@Override
-			protected void processWindowEvent(WindowEvent e) {
-				if (e.getID() != WindowEvent.WINDOW_CLOSING)
-					super.processWindowEvent(e);
-			}
-		};
-		mFrame.getContentPane().setForeground(new Color(255, 255, 255));
+        mFrame = new JFrame() {
+            private static final long serialVersionUID = 1L;
 
-		mFrame.setBounds(700, 400, 700, 510);// 窗口居中
-		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            @Override
+            protected void processWindowEvent(WindowEvent e) {
+                if (e.getID() != WindowEvent.WINDOW_CLOSING)
+                    super.processWindowEvent(e);
+            }
+        };
+        mFrame.getContentPane().setForeground(new Color(255, 255, 255));
 
-		// Call panel
-		mPanel.setName("Call");
-		mPanel.setLayout(null);
-		mFrame.add(mPanel);
-		mPanel.setBounds(500, 0, 500, 0);
-		mPanel.setBackground(new Color(250, 250, 250));
+        mFrame.setBounds(700, 400, 700, 510);// 窗口居中
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		mBtnStart.setLabel("start");
-		mBtnStart.setBackground(new Color(64, 147, 77));
-		mPanel.add(mBtnStart);
-		mBtnStart.setBounds(10, 20, 50, 30);
+        // Call panel
+        mPanel.setName("Call");
+        mPanel.setLayout(null);
+        mFrame.add(mPanel);
+        mPanel.setBounds(500, 0, 500, 0);
+        mPanel.setBackground(new Color(250, 250, 250));
 
-		mBtnStop.setLabel("stop");
-		mBtnStop.setBackground(new Color(176, 196, 222));
-		mPanel.add(mBtnStop);
-		mBtnStop.setBounds(10, 60, 50, 30);
+        mBtnStart.setLabel("start");
+        mBtnStart.setBackground(new Color(64, 147, 77));
+        mPanel.add(mBtnStart);
+        mBtnStart.setBounds(10, 20, 50, 30);
 
-		mBtnClear.setLabel("clear");
-		mBtnClear.setBackground(new Color(176, 196, 222));
-		mPanel.add(mBtnClear);
-		mBtnClear.setBounds(10, 100, 50, 30);
+        mBtnStop.setLabel("stop");
+        mBtnStop.setBackground(new Color(176, 196, 222));
+        mPanel.add(mBtnStop);
+        mBtnStop.setBounds(10, 60, 50, 30);
 
-		mBtnLock.setLabel("lock");
-		mBtnLock.setBackground(new Color(176, 196, 222));
-		mPanel.add(mBtnLock);
-		mBtnLock.setBounds(10, 140, 50, 30);
+        mBtnClear.setLabel("clear");
+        mBtnClear.setBackground(new Color(176, 196, 222));
+        mPanel.add(mBtnClear);
+        mBtnClear.setBounds(10, 100, 50, 30);
 
-		mBtnFlush.setLabel("flush");
-		mBtnFlush.setBackground(new Color(176, 196, 222));
-		mPanel.add(mBtnFlush);
-		mBtnFlush.setBounds(10, 180, 50, 30);
+        mBtnLock.setLabel("lock");
+        mBtnLock.setBackground(new Color(176, 196, 222));
+        mPanel.add(mBtnLock);
+        mBtnLock.setBounds(10, 140, 50, 30);
 
-		mTvLogs.setBounds(70, 10, 600, 340);
-		mPanel.add(mTvLogs);
+        mBtnFlush.setLabel("flush");
+        mBtnFlush.setBackground(new Color(176, 196, 222));
+        mPanel.add(mBtnFlush);
+        mBtnFlush.setBounds(10, 180, 50, 30);
 
-		// mTvDevices.setBounds(70, 360, 380, 100);
-		// mPanel.add(mTvDevices);
+        mTvLogs.setBounds(70, 10, 600, 340);
+        mPanel.add(mTvLogs);
 
-		/**
-		 * 改动
-		 */
-		mScrollPane.setBounds(70, 360, 380, 100);
-		mPanel.add(mScrollPane);
+        // mTvDevices.setBounds(70, 360, 380, 100);
+        // mPanel.add(mTvDevices);
 
-		mTvDatas.setBounds(460, 360, 210, 100);
-		mPanel.add(mTvDatas);
+        /**
+         * 改动
+         */
+        mScrollPane.setBounds(70, 360, 380, 100);
+        mPanel.add(mScrollPane);
 
-		// mTfPort.setBounds(10, 430, 50, 30);
-		// mTfPort.setText("9114");
-		// mPanel.add(mTfPort);
+        mTvDatas.setBounds(460, 360, 210, 100);
+        mPanel.add(mTvDatas);
 
-		mLbCount.setBounds(620, 455, 100, 30);
-		mLbCount.setText("online:0");
-		mPanel.add(mLbCount);
+        // mTfPort.setBounds(10, 430, 50, 30);
+        // mTfPort.setText("9114");
+        // mPanel.add(mTfPort);
 
-		//
-		JavaPhoneMouse aJavaPhoneMouse = new JavaPhoneMouse();
-		mBtnStart.addMouseListener(aJavaPhoneMouse);
-		mBtnStop.addMouseListener(aJavaPhoneMouse);
-		mBtnClear.addMouseListener(aJavaPhoneMouse);
-		mBtnLock.addMouseListener(aJavaPhoneMouse);
-		mBtnFlush.addMouseListener(aJavaPhoneMouse);
+        mLbCount.setBounds(620, 455, 100, 30);
+        mLbCount.setText("online:0");
+        mPanel.add(mLbCount);
+        //
+        mProgressCount.setBounds(530, 465, 630, 10);
+        mProgressCount.setText("0/0");
+        mPanel.add(mProgressCount);
+        //
+        mProgressBar.setBounds(70, 470, 450, 5);
+        mPanel.add(mProgressBar);
+        //
+        JavaPhoneMouse aJavaPhoneMouse = new JavaPhoneMouse();
+        mBtnStart.addMouseListener(aJavaPhoneMouse);
+        mBtnStop.addMouseListener(aJavaPhoneMouse);
+        mBtnClear.addMouseListener(aJavaPhoneMouse);
+        mBtnLock.addMouseListener(aJavaPhoneMouse);
+        mBtnFlush.addMouseListener(aJavaPhoneMouse);
 
-	}
+    }
 
-	// 事件監聽
-	class JavaPhoneMouse extends java.awt.event.MouseAdapter {
-		public void mouseClicked(java.awt.event.MouseEvent event) {
-			Object object = event.getSource();
-			if (object == mBtnStart) {
-				clickListener.onStart();
+    // 事件監聽
+    class JavaPhoneMouse extends java.awt.event.MouseAdapter {
+        public void mouseClicked(java.awt.event.MouseEvent event) {
+            Object object = event.getSource();
+            if (object == mBtnStart) {
+                clickListener.onStart();
 
-			}
-			if (object == mBtnStop) {
-				clickListener.onStop();
-			}
-			if (object == mBtnClear) {
-				mTvLogs.setText("");
-			}
-			if (object == mBtnLock) {
+            }
+            if (object == mBtnStop) {
+                clickListener.onStop();
+            }
+            if (object == mBtnClear) {
+                mTvLogs.setText("");
+            }
+            if (object == mBtnLock) {
 
-				isLock = !isLock;
-				mBtnLock.setLabel(isLock ? "unLock" : "lock");
-				if (isLock) {
-					ELog.getInstance().setOutputSource(null);
-				} else {
-					ELog.getInstance().setOutputSource(mTvLogs);
-				}
-			}
-			if (object == mBtnFlush) {
-				clickListener.onFlush();
-			}
-		}
-	}
+                isLock = !isLock;
+                mBtnLock.setLabel(isLock ? "unLock" : "lock");
+                if (isLock) {
+                    ELog.getInstance().setOutputSource(null);
+                } else {
+                    ELog.getInstance().setOutputSource(mTvLogs);
+                }
+            }
+            if (object == mBtnFlush) {
+                clickListener.onFlush();
+            }
+        }
+    }
 
-	public interface OnClickListener {
+    public interface OnClickListener {
 
-		void onInit(TextArea contentText, Label countLabel, TextArea mTvDatas, TextField mTfPort, JList<String> mJList);
+        void onInit(TextArea contentText, Label countLabel, TextArea mTvDatas, TextField mTfPort, JList<String> mJList, final JProgressBar pb,
+                    final Label pl);
 
-		void onStart();
+        void onStart();
 
-		void onStop();
+        void onStop();
 
-		void onFlush();
+        void onFlush();
 
-		void onReset();
+        void onReset();
 
-	}
+    }
 
-	public interface OnStatusChangeListener {
-		void onConnectChange();
+    public interface OnStatusChangeListener {
+        void onConnectChange();
 
-		void onDataChanged(/*
-							 * Map<Integer, Map<Integer, AbsJsonConvert<?>>>
-							 * mBuffer
-							 */);
-	}
+        void onDataChanged();
+
+        void onProgressChange(int current, int total);
+    }
 }
