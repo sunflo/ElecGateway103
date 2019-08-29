a36f1255736e49b0754ab775829cbe9472cce554
huangxz <huangxiaozheng@kedacom.com>
2019-08-29 17:25:07 +0800

    DK放入config管理

--- src/main/java/com/heshun/dsm/common/Config.java
+++ src/main/java/com/heshun/dsm/common/Config.java
@@ -1,6 +1,6 @@
 package com.heshun.dsm.common;
 
 public class Config {
-	public static boolean isDebug = true;
-
+    public static boolean isDebug = false;
+    public static int[] DK = new int[]{5, 8, 7, 2};
 }
